5c2bfcefb1d00a115ef199bbeba5ab14032433da
Mykhailo <mykhailoevo@gmail.com>
2024-09-02 15:27:01 +0300

    add readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
+  root: true,
   extends: ['@mate-academy/eslint-config-react-typescript'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
